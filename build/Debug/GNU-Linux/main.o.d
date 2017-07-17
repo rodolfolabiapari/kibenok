ce62fde8ea19077dced9f5ed708396adbde26fed
rodolfolabiapari <rodolfolabiapari@gmail.com>
2017-07-17 14:53:34 -0300

    Consertar o problema de duplicar a imagem.
    Acho que é antes do processamento do cvDFT.

---
+++ build/Debug/GNU-Linux/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-Linux/main.o: main.cpp filter_procedure.h
+
+filter_procedure.h:
--- main.cpp
+++ main.cpp
@@ -21,29 +21,207 @@
 
 #include "filter_procedure.h"
 
-void print_ipl(IplImage * ipl) 
+
+void cvShiftDFT(CvArr * src_arr, CvArr * dst_arr )
+{
+    CvMat * tmp;
+    CvMat q1stub, q2stub;
+    CvMat q3stub, q4stub;
+    CvMat d1stub, d2stub;
+    CvMat d3stub, d4stub;
+    CvMat * q1, * q2, * q3, * q4;
+    CvMat * d1, * d2, * d3, * d4;
+
+    CvSize size = cvGetSize(src_arr);
+    CvSize dst_size = cvGetSize(dst_arr);
+    int cx, cy;
+
+    if(dst_size.width != size.width ||
+       dst_size.height != size.height){
+        cvError( CV_StsUnmatchedSizes, "cvShiftDFT", "Source and Destination arrays must have equal sizes", __FILE__, __LINE__ );  
+    }
+
+    if(src_arr==dst_arr){
+        tmp = cvCreateMat(size.height/2, size.width/2,
+cvGetElemType(src_arr));
+    }
+   
+    cx = size.width/2;
+    cy = size.height/2; // image center
+
+    q1 = cvGetSubRect( src_arr, &q1stub, cvRect(0,0,cx, cy) );
+    q2 = cvGetSubRect( src_arr, &q2stub, cvRect(cx,0,cx,cy) );
+    q3 = cvGetSubRect( src_arr, &q3stub, cvRect(cx,cy,cx,cy) );
+    q4 = cvGetSubRect( src_arr, &q4stub, cvRect(0,cy,cx,cy) );
+    d1 = cvGetSubRect( src_arr, &d1stub, cvRect(0,0,cx,cy) );
+    d2 = cvGetSubRect( src_arr, &d2stub, cvRect(cx,0,cx,cy) );
+    d3 = cvGetSubRect( src_arr, &d3stub, cvRect(cx,cy,cx,cy) );
+    d4 = cvGetSubRect( src_arr, &d4stub, cvRect(0,cy,cx,cy) );
+
+    if(src_arr!=dst_arr){
+        if( !CV_ARE_TYPES_EQ( q1, d1 )){
+            cvError( CV_StsUnmatchedFormats, "cvShiftDFT", "Source and Destination arrays must have the same format", __FILE__, __LINE__ );
+        }
+        cvCopy(q3, d1, 0);
+        cvCopy(q4, d2, 0);
+        cvCopy(q1, d3, 0);
+        cvCopy(q2, d4, 0);
+    }
+    else{
+        cvCopy(q3, tmp, 0);
+        cvCopy(q1, q3, 0);
+        cvCopy(tmp, q1, 0);
+        cvCopy(q4, tmp, 0);
+        cvCopy(q2, q4, 0);
+        cvCopy(tmp, q2, 0);
+    }
+}
+
+
+void ipl_print(IplImage * ipl) 
 {
 	int i, j;
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			printf("%6.3f ", (double) ipl->imageData[i * ipl->width + j]);
+			printf("%6.3f ", ((float*) ipl->imageData)[i * ipl->width + j]);
 			fflush(stdout);
 		}
 		printf("\n");
 	}
 }
 
-void fill_ipl(IplImage * ipl, double v) 
+void ipl_print_char(IplImage * ipl) 
 {
 	int i, j;
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			ipl->imageData[i * ipl->width + j] = v;
+			printf("%d ", ((unsigned char*) ipl->imageData)[i * ipl->width + j]);
+			fflush(stdout);
+		}
+		printf("\n");
+	}
+}
+
+void ipl_fill(IplImage * ipl, float v) 
+{
+	int i, j;
+	
+	for (i = 0; i < ipl->height; i++) {
+		for (j = 0; j < ipl->width; j++) {
+			((float * ) ipl->imageData)[i * ipl->width + j] = v;
 		}
 	}
 }
+
+
+void ipl_copy_float(IplImage * src, IplImage * dst) 
+{
+	int i, j;
+	
+	for (i = 0; i < src->height; i++) {
+		for (j = 0; j < src->width; j++) {
+			((float * ) dst->imageData)[i * dst->width + j] = ((unsigned char * ) src->imageData)[i * src->width + j];
+		}
+	}
+}
+
+void ipl_copy_char(IplImage * src, IplImage * dst) 
+{
+	int i, j;
+	
+	for (i = 0; i < src->height; i++) {
+		for (j = 0; j < src->width; j++) {
+			((unsigned char * ) dst->imageData)[i * dst->width + j] = ((float * ) src->imageData)[i * src->width + j];
+		}
+	}
+}
+
+void ipl_divide_scalar(IplImage * ipl, float div) 
+{
+	int i, j;
+	
+	for (i = 0; i < ipl->height; i++) {
+		for (j = 0; j < ipl->width; j++) {
+			((float * ) ipl->imageData)[i * ipl->width + j] = ((float * ) ipl->imageData)[i * ipl->width + j] / div;
+		}
+	}
+}
+
+
+float scale_number(float t, float minOld, float maxOld, float minNew, float maxNew) {
+	float r = 0;
+	r = minNew + ((maxNew - minNew) / (maxOld - minOld)) * (t - minOld);
+	return r;
+}
+
+void scale (IplImage * ipl, /*float minOld, float maxOld,*/ float minNew, float maxNew) {
+	
+	int i, j;
+	float minOld, maxOld;
+	
+	minOld = maxOld = ((float * ) ipl->imageData)[0];
+	
+	for (i = 0; i < ipl->height; i++) {
+		for (j = 0; j < ipl->width; j++) {
+			if (maxOld < ((float * ) ipl->imageData)[i * ipl->width + j]) maxOld = ((float * ) ipl->imageData)[i * ipl->width + j];
+			if (minOld > ((float * ) ipl->imageData)[i * ipl->width + j]) minOld = ((float * ) ipl->imageData)[i * ipl->width + j];
+		}
+	}
+	
+	for (i = 0; i < ipl->height; i++) {
+		for (j = 0; j < ipl->width; j++) {
+			((float * ) ipl->imageData)[i * ipl->width + j] = scale_number(
+					  ((float * ) ipl->imageData)[i * ipl->width + j], minOld, maxOld, minNew, maxNew);
+		}
+	}
+}
+
+void show_image(IplImage * ipl_in_complex) {
+	
+	IplImage * ipl_dft_real, *  ipl_dft_imaginary, * ipl_image;
+	double min = 0, max = 0;
+	
+	ipl_dft_real      = cvCreateImage(cvGetSize(ipl_in_complex), IPL_DEPTH_64F, 1);
+	ipl_dft_imaginary = cvCreateImage(cvGetSize(ipl_in_complex), IPL_DEPTH_64F, 1);
+	ipl_image         = cvCreateImage(cvGetSize(ipl_in_complex), IPL_DEPTH_8U,  1);
+
+	
+	// Split Fourier in real and imaginary parts
+	cvSplit( ipl_in_complex, ipl_dft_real, ipl_dft_imaginary, NULL, NULL);
+
+	// Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
+	cvPow( ipl_dft_real, ipl_dft_real, 2.0);
+	cvPow( ipl_dft_imaginary, ipl_dft_imaginary, 2.0);
+	cvAdd( ipl_dft_real, ipl_dft_imaginary, ipl_dft_real, NULL);
+	cvPow( ipl_dft_real, ipl_dft_real, 0.5 );
+
+	// Compute log(1 + Mag)
+	cvAddS( ipl_dft_real, cvScalarAll(1.0), ipl_dft_real, NULL ); // 1 + Mag
+	cvLog( ipl_dft_real, ipl_dft_real ); // log(1 + Mag)
+	
+
+	// Rearrange the quadrants of Fourier image so that the origin is at
+	// the image center
+	//cvShiftDFT( ipl_dft_real, ipl_dft_real );
+
+	cvMinMaxLoc(ipl_dft_real, &min, &max, NULL, NULL, NULL);
+	printf("Min:%f Max:%f\n", min, max);
+	cvScale(ipl_dft_real, ipl_dft_real, 1.0/(max-min), 1.0*(-min)/(max-min));
+	
+	cvMinMaxLoc(ipl_dft_real, &min, &max, NULL, NULL, NULL);
+	printf("Min:%f Max:%f\n", min, max);
+	//ipl_print_char(ipl_dft_real);
+	
+	//ipl_copy_char(ipl_dft_real, ipl_image);
+	
+	cvNamedWindow("DFT", 0);
+	cvShowImage("DFT", ipl_dft_real);
+	
+	cvWaitKey(0);
+}
+
 
 /*
  * 
@@ -69,8 +247,7 @@ int main(int argc, char** argv)
 	int PLAN_ROWS = 0, PLAN_COLS = 0;
 	IplImage * ipl_in_255 = 0, * ipl_out = 0,
 			  * ipl_in_real = 0, * ipl_in_imaginary = 0, 
-			  * ipl_in_complex = 0;
-	IplImage * ipl_dft_real, *  ipl_dft_imaginary, * div;
+			  * ipl_in_complex = 0, * div = 0;
 	CvMat * plan, temp;
 
 	ipl_in_255 = cvLoadImage(path, CV_LOAD_IMAGE_GRAYSCALE);
@@ -87,10 +264,12 @@ int main(int argc, char** argv)
 	ipl_in_complex   = cvCreateImage(cvGetSize(ipl_in_255), IPL_DEPTH_64F, 2);
 	div              = cvCreateImage(cvGetSize(ipl_in_255), IPL_DEPTH_64F, 1);
 	
-	plan              = cvCreateMat(PLAN_ROWS, PLAN_COLS, CV_64FC2);
+	//plan              = cvCreateMat(PLAN_ROWS, PLAN_COLS, CV_64FC2);
 	
-	ipl_dft_real      = cvCreateImage(cvGetSize(ipl_in_255), IPL_DEPTH_64F, 1);
-	ipl_dft_imaginary = cvCreateImage(cvGetSize(ipl_in_255), IPL_DEPTH_64F, 1);
+	cvZero(ipl_in_real);
+	cvZero(ipl_in_imaginary);
+	cvZero(ipl_in_complex);
+	cvZero(div);
 	
 	PLAN_COLS = cvGetOptimalDFTSize(ipl_in_255->height - 1);
 	PLAN_ROWS = cvGetOptimalDFTSize(ipl_in_255->width  - 1);
@@ -120,52 +299,22 @@ int main(int argc, char** argv)
 		cvZero(&temp);
 	}*/
 	
-	//cvSet( div, cvScalar(120.0), NULL);
 	
-	fill_ipl(div, 255.0);
+	ipl_copy_float(ipl_in_255, ipl_in_real);
+	//ipl_print(ipl_in_real);
 	
-	cvConvert(ipl_in_255, ipl_in_real);
+	ipl_fill(div, (float) 255);
 	
-	print_ipl(ipl_in_real);
+	ipl_divide_scalar(ipl_in_real, 255.0);
+	//ipl_print(ipl_in_real);
 	
-	cvDiv(ipl_in_real, div, ipl_in_real, 0);
-	
-	cvZero(ipl_in_imaginary);
 	cvMerge(ipl_in_real, ipl_in_imaginary, NULL, NULL, ipl_in_complex);
 	
-	cvDFT(ipl_in_complex, ipl_in_complex, (CV_DXT_FORWARD | CV_DXT_ROWS));  
+	cvDFT(ipl_in_complex, ipl_in_complex, (CV_DXT_FORWARD));  
 	
-	
+	//show_image(ipl_in_complex, 1);
 	
 	//cvDFT(ipl_in_complex, ipl_in_complex, CV_DXT_FORWARD, ipl_in_complex->height);
-
-
-
-
-
-	// Split Fourier in real and imaginary parts
-	cvSplit( ipl_in_complex, ipl_dft_real, ipl_dft_imaginary, NULL, NULL);
-
-	// Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
-	cvPow( ipl_dft_real, ipl_dft_real, 2.0);
-	cvPow( ipl_dft_imaginary, ipl_dft_imaginary, 2.0);
-	cvAdd( ipl_dft_real, ipl_dft_imaginary, ipl_dft_real, NULL);
-	cvPow( ipl_dft_real, ipl_dft_real, 0.5 );
-
-	// Compute log(1 + Mag)
-	cvAddS( ipl_dft_real, cvScalarAll(1.0), ipl_dft_real, NULL ); // 1 + Mag
-	cvLog( ipl_dft_real, ipl_dft_real ); // log(1 + Mag)
-
-
-	// Rearrange the quadrants of Fourier image so that the origin is at
-	// the image center
-	//cvShiftDFT( ipl_dft_real, ipl_dft_real );
-
-	cvMinMaxLoc(ipl_dft_real, &min, &max, NULL, NULL, NULL);
-	cvScale(ipl_dft_real, ipl_dft_real, 1.0/(max-min), 1.0*(-min)/(max-min));
-	
-	cvNamedWindow("DFT", 0);
-	cvShowImage("DFT", ipl_dft_real);
 
 	
 	
@@ -195,7 +344,6 @@ int main(int argc, char** argv)
 	/*cvSaveImage("In.png", image_in, 0);
 	cvSaveImage("Out.png", image_out, 0);*/
 	
-	cvWaitKey(0);
     /*
 	cvShowImage("iplimage_dft(): mag", image_mag);
 	cvShowImage("iplimage_dft(): phase", image_phase);
@@ -214,6 +362,14 @@ int main(int argc, char** argv)
 	cvReleaseImage(&image_spectrum);
 	 */
 	
+	
+	
+	
+	cvDFT(ipl_in_complex, ipl_in_complex, CV_DXT_INVERSE_SCALE);
+	
+	ipl_print(ipl_in_complex);
+	
+	show_image(ipl_in_complex);
 	
 	
 	printf("\tCleaning the images\n");

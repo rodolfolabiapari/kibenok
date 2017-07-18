48974cb2e331cb6085328a591ed06c39b366f982
rodolfolabiapari <rodolfolabiapari@gmail.com>
2017-07-18 14:35:13 -0300

    FUNCIONOUOOOUOUOUO

--- main.cpp
+++ main.cpp
@@ -78,13 +78,13 @@ cvGetElemType(src_arr));
 }
 
 
-void mat_print(CvMat * mat) 
+void mat_print_double(CvMat * mat) 
 {
 	int i, j;
 	
 	for (i = 0; i < mat->rows; i++) {
 		for (j = 0; j < mat->cols; j++) {
-			printf("%6.3f ", ((float*) mat->data.ptr)[i * mat->cols + j]);
+			printf("%4d: %6.3f\n", i * mat->width + j, ((double*) mat->data.ptr)[i * mat->cols + j]);
 			fflush(stdout);
 		}
 		printf("\n");
@@ -97,19 +97,19 @@ void mat_print_char(CvMat * mat)
 	
 	for (i = 0; i < mat->rows; i++) {
 		for (j = 0; j < mat->cols; j++) {
-			printf("%d ", ((unsigned char*) mat->data.ptr)[i * mat->cols + j]);
+			printf("%4d ", ((unsigned char*) mat->data.ptr)[i * mat->cols + j]);
 			fflush(stdout);
 		}
 		printf("\n");
 	}
 }
-void ipl_print(IplImage * ipl) 
+void ipl_print_double(IplImage * ipl) 
 {
 	int i, j;
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			printf("%6.3f ", ((float*) ipl->imageData)[i * ipl->width + j]);
+			printf("%4d: %6.3f\n", i * ipl->width + j, ((double*) ipl->imageData)[i * ipl->width + j]);
 			fflush(stdout);
 		}
 		printf("\n");
@@ -122,7 +122,7 @@ void ipl_print_char(IplImage * ipl)
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			printf("%d ", ((unsigned char*) ipl->imageData)[i * ipl->width + j]);
+			printf("%4d: %d\n", i * ipl->width + j, ((unsigned char*) ipl->imageData)[i * ipl->width + j]);
 			fflush(stdout);
 		}
 		printf("\n");
@@ -141,13 +141,13 @@ void ipl_fill(IplImage * ipl, float v)
 }
 
 
-void ipl_copy_float(IplImage * src, IplImage * dst) 
+void ipl_copy_double(IplImage * src, IplImage * dst) 
 {
 	int i, j;
 	
 	for (i = 0; i < src->height; i++) {
 		for (j = 0; j < src->width; j++) {
-			((float * ) dst->imageData)[i * dst->width + j] = ((unsigned char * ) src->imageData)[i * src->width + j];
+			((double * ) dst->imageData)[i * dst->width + j] = ((unsigned char * ) src->imageData)[i * src->width + j];
 		}
 	}
 }
@@ -158,18 +158,18 @@ void ipl_copy_char(IplImage * src, IplImage * dst)
 	
 	for (i = 0; i < src->height; i++) {
 		for (j = 0; j < src->width; j++) {
-			((unsigned char * ) dst->imageData)[i * dst->width + j] = ((float * ) src->imageData)[i * src->width + j];
+			((unsigned char * ) dst->imageData)[i * dst->width + j] = ((double * ) src->imageData)[i * src->width + j];
 		}
 	}
 }
 
-void ipl_divide_scalar(IplImage * ipl, float div) 
+void ipl_divide_scalar(IplImage * ipl, double div) 
 {
 	int i, j;
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			((float * ) ipl->imageData)[i * ipl->width + j] = ((float * ) ipl->imageData)[i * ipl->width + j] / div;
+			((double * ) ipl->imageData)[i * ipl->width + j] = ((double * ) ipl->imageData)[i * ipl->width + j] / div;
 		}
 	}
 }
@@ -181,52 +181,52 @@ float scale_number(float t, float minOld, float maxOld, float minNew, float maxN
 	return r;
 }
 
-void scale (IplImage * ipl, /*float minOld, float maxOld,*/ float minNew, float maxNew) {
-	
+void scale (IplImage * ipl, /*float minOld, float maxOld,*/ double minNew, double maxNew) {
 	int i, j;
 	float minOld, maxOld;
 	
-	minOld = maxOld = ((float * ) ipl->imageData)[0];
+	minOld = maxOld = ((double * ) ipl->imageData)[0];
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			if (maxOld < ((float * ) ipl->imageData)[i * ipl->width + j]) maxOld = ((float * ) ipl->imageData)[i * ipl->width + j];
-			if (minOld > ((float * ) ipl->imageData)[i * ipl->width + j]) minOld = ((float * ) ipl->imageData)[i * ipl->width + j];
+			if (maxOld < ((double * ) ipl->imageData)[i * ipl->width + j]) maxOld = ((double * ) ipl->imageData)[i * ipl->width + j];
+			if (minOld > ((double * ) ipl->imageData)[i * ipl->width + j]) minOld = ((double * ) ipl->imageData)[i * ipl->width + j];
 		}
 	}
 	
 	for (i = 0; i < ipl->height; i++) {
 		for (j = 0; j < ipl->width; j++) {
-			((float * ) ipl->imageData)[i * ipl->width + j] = scale_number(
-					  ((float * ) ipl->imageData)[i * ipl->width + j], minOld, maxOld, minNew, maxNew);
+			((double * ) ipl->imageData)[i * ipl->width + j] = scale_number(
+					  ((double * ) ipl->imageData)[i * ipl->width + j], minOld, maxOld, minNew, maxNew);
 		}
 	}
 }
 
 
-void normalize_merge (CvArr * arr, /*float minOld, float maxOld,*/ float minNew, float maxNew) {
-	
+void normalize_merge (CvMat * mat) {
 	int i, j;
 	
-	for (i = 0; i < ipl->height; i+=4) {
-		for (j = 0; j < ipl->width; j+=4) {
-			for (k = )
+	for (i = 0; i < mat->height; i+=4) {
+		for (j = 0; j < mat->width; j+=4) {
+			mat->data.ptr[i * mat->width + j + 1] = mat->data.ptr[i * mat->width + j + 0];
+			mat->data.ptr[i * mat->width + j + 2] = mat->data.ptr[i * mat->width + j + 0];
+			mat->data.ptr[i * mat->width + j + 3] = mat->data.ptr[i * mat->width + j + 0];
 		}
 	}
 }
 
-void show_image(CvArr * ipl_in_complex) {
+void show_image(CvArr * in_complex, char isFORWARD) {
 	
 	IplImage * ipl_dft_real, *  ipl_dft_imaginary, * ipl_image;
 	double min = 0, max = 0;
 	
-	ipl_dft_real      = cvCreateImage(cvGetSize(ipl_in_complex), IPL_DEPTH_64F, 1);
-	ipl_dft_imaginary = cvCreateImage(cvGetSize(ipl_in_complex), IPL_DEPTH_64F, 1);
-	ipl_image         = cvCreateImage(cvGetSize(ipl_in_complex), IPL_DEPTH_8U,  1);
+	ipl_dft_real      = cvCreateImage(cvGetSize(in_complex), IPL_DEPTH_64F, 1);
+	ipl_dft_imaginary = cvCreateImage(cvGetSize(in_complex), IPL_DEPTH_64F, 1);
+	ipl_image         = cvCreateImage(cvGetSize(in_complex), IPL_DEPTH_8U,  1);
 
 	
 	// Split Fourier in real and imaginary parts
-	cvSplit( ipl_in_complex, ipl_dft_real, ipl_dft_imaginary, NULL, NULL);
+	cvSplit( in_complex, ipl_dft_real, ipl_dft_imaginary, NULL, NULL);
 
 	// Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
 	cvPow( ipl_dft_real, ipl_dft_real, 2.0);
@@ -235,8 +235,10 @@ void show_image(CvArr * ipl_in_complex) {
 	cvPow( ipl_dft_real, ipl_dft_real, 0.5 );
 
 	// Compute log(1 + Mag)
-	cvAddS( ipl_dft_real, cvScalarAll(1.0), ipl_dft_real, NULL ); // 1 + Mag
-	cvLog( ipl_dft_real, ipl_dft_real ); // log(1 + Mag)
+	if (isFORWARD) {
+		cvAddS( ipl_dft_real, cvScalarAll(1.0), ipl_dft_real, NULL ); // 1 + Mag
+		cvLog( ipl_dft_real, ipl_dft_real ); // log(1 + Mag)
+	}
 	
 
 	// Rearrange the quadrants of Fourier image so that the origin is at
@@ -249,7 +251,7 @@ void show_image(CvArr * ipl_in_complex) {
 	
 	cvMinMaxLoc(ipl_dft_real, &min, &max, NULL, NULL, NULL);
 	printf("Min:%f Max:%f\n", min, max);
-	//ipl_print_char(ipl_dft_real);
+	//ipl_print_double(ipl_dft_real);
 	
 	//ipl_copy_char(ipl_dft_real, ipl_image);
 	
@@ -267,74 +269,54 @@ int main(int argc, char** argv)
 	// http://opencv-users.1802565.n2.nabble.com/faint-image-after-Inverse-DFT-using-cvDFT-td2193072.html
 	// https://stackoverflow.com/questions/21984413/different-results-with-cvdft-and-dft-in-opencv-2-4-8
 	
-	//int WIDTH, HEIGHT, DIM;
-
-	//const char * path = "bw/gray.jpeg";
-	//const char * path = "grad.png";
-	//const char * path = "color/lenna.jpg";
+	
 	const char * path = "lenna.png";
-	//const char * path = "xadrez.png";
-	//const char * path = "tel.jpg";
-	//const char * path = "bw/telb.png";
-	//const char * path = "collor.jpg";
-	//const char * path = "bw/tinyb.jpg";
 
 	int ROWS = 0, COLS = 0;
-	IplImage * ipl_in_255 = 0, * ipl_out = 0,
-			  * ipl_in_real = 0, * ipl_in_imaginary = 0, 
-			  * ipl_in_complex = 0, * div = 0;
-	CvMat * plan, temp;
-
-	ipl_in_255 = cvLoadImage(path, CV_LOAD_IMAGE_GRAYSCALE);
 	
-	if (ipl_in_255 == 0) {
+	IplImage * ipl_8_in_255 = 0, * ipl_out = 0,
+			  * ipl_64_in_real = 0, * ipl_64_in_imaginary = 0, 
+			  * ipl_64c2_in_complex = 0, * ipl_64_div = 0;
+	CvMat * mat_64c2_plan, temp;
+
+	ipl_8_in_255 = cvLoadImage(path, CV_LOAD_IMAGE_GRAYSCALE);
+	
+	if (ipl_8_in_255 == 0) {
 		printf("\nERROR! Image not loaded. Check in main function\n");
 		exit(-1);
 	}
 	
-	COLS = cvGetOptimalDFTSize(ipl_in_255->height - 1);
-	ROWS = cvGetOptimalDFTSize(ipl_in_255->width  - 1);
+	COLS = cvGetOptimalDFTSize(ipl_8_in_255->height - 1);
+	ROWS = cvGetOptimalDFTSize(ipl_8_in_255->width  - 1);
 
 	// Allocate the images and set the parameters values
-	ipl_in_real      = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 1);
+	ipl_64_in_real      = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 1);
 	
-	ipl_in_imaginary = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 1);
-	ipl_in_complex   = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 2);
-	div              = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 1);
+	ipl_64_in_imaginary = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 1);
+	ipl_64c2_in_complex = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 2);
+	ipl_64_div          = cvCreateImage(cvSize(COLS, ROWS), IPL_DEPTH_64F, 1);
 	
-	plan             = cvCreateMat(ROWS, COLS, CV_64FC2);
-	
-	
-	int i = 0, j = 0;
-	
-	for (i = 0; i < ipl_in_255->height; i++) {
-		for (j = 0; j < ipl_in_255->width; j++) {
-			ipl_in_real->imageData[i * ipl_in_255->width + j]      = 0;
-			ipl_in_imaginary->imageData[i * ipl_in_255->width + j] = 0;
-			ipl_in_complex->imageData[i * ipl_in_255->width + j]   = 0;
-			div->imageData[i * ipl_in_255->width + j]              = 0;
-			plan->data.ptr[i * ipl_in_255->width + j]              = 0;
-		}
-	}
+	mat_64c2_plan       = cvCreateMat(ROWS, COLS, CV_64FC2);
 	
 	
-	ipl_copy_float(ipl_in_255, ipl_in_real);
+	
+	ipl_copy_double(ipl_8_in_255, ipl_64_in_real);
 	//ipl_print(ipl_in_real);
 	
-	ipl_fill(div, (float) 255);
+	ipl_fill(ipl_64_div, (float) 255);
 	
-	ipl_divide_scalar(ipl_in_real, 255.0);
+	ipl_divide_scalar(ipl_64_in_real, 255.0);
 	//ipl_print(ipl_in_real);
 	
-	cvMerge(ipl_in_real, ipl_in_imaginary, NULL, NULL, ipl_in_complex);
+	cvMerge(ipl_64_in_real, ipl_64_in_imaginary, NULL, NULL, ipl_64c2_in_complex);
 	
 	//ipl_print(ipl_in_complex);
 	
-	CvRect window = cvRect(0, 0, ipl_in_255->width, ipl_in_255->height);
+	CvRect window = cvRect(0, 0, ipl_8_in_255->width, ipl_8_in_255->height);
 	
 	
 	// Set the plan for operations on Fourier's transform
-	cvGetSubRect(ipl_in_complex, plan, window);
+	cvGetSubRect(ipl_64c2_in_complex, mat_64c2_plan, window);
 	//cvCopy(&temp, plan, NULL);
 	
 	
@@ -346,14 +328,11 @@ int main(int argc, char** argv)
 	
 
 	
-	cvDFT(plan, plan, (CV_DXT_FORWARD));  
+	cvDFT(mat_64c2_plan, mat_64c2_plan, (CV_DXT_FORWARD));  
 	
-	//mat_print(plan);
-	
-	//show_image(ipl_in_complex, 1);
+	//show_image(mat_64c2_plan);
 	
 	//cvDFT(ipl_in_complex, ipl_in_complex, CV_DXT_FORWARD, ipl_in_complex->height);
-
 	
 	
 	
@@ -403,13 +382,17 @@ int main(int argc, char** argv)
 	
 	
 	
-	cvDFT(plan, plan , CV_DXT_INVERSE);
+	cvDFT(mat_64c2_plan, mat_64c2_plan , CV_DXT_INVERSE);
 	
-	mat_print_char(plan);
+	//mat_print_double(mat_64c2_plan);
+	
+	//normalize_merge(mat_64c2_plan);
+	
+	//mat_print_double(mat_64c2_plan);
 	
 	//ipl_print(ipl_in_complex);
 	
-	show_image(plan);
+	show_image(mat_64c2_plan, 0);
 	
 	
 	printf("\tCleaning the images\n");
